2c0b104110ec4c8c4646dba47805384283e508e8
Leaptail <52641374+Leaptail@users.noreply.github.com>
2023-10-03 17:28:18 +0800

    finding multiple trees on screen

--- main.py
+++ main.py
@@ -9,12 +9,52 @@ haystack_img = cv.imread('farm.PNG', cv.IMREAD_UNCHANGED)
 needle_img = cv.imread('memories/Tree/Tree1.PNG', cv.IMREAD_UNCHANGED)
 
 result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
+print(result)
+
+threshold = 0.3
+locations = np.where(result >= threshold)
+print(locations)
+
+locations = list(zip(*locations[::-1]))
+print(locations)
+
+if locations:
+    print('found')
+
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
+    line_color = (255,0,0)
+    line_type = cv.LINE_4
+
+    for loc in locations:
+        top_left = loc
+        bottom_right = (top_left[0]+needle_w,top_left[1]+needle_h)
+        cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
+
+    cv.imshow('Result', haystack_img)
+    cv.waitKey()
+else:
+    print('not found')
+
+
+
+
+
+
 #Debug code
 #cv.imshow('Result', result)
 #cv.waitKey()
 
+
+
+
+
+
+
+
+
 #min max of brightest and darkest pixel, get best match position
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+"""min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
 
 print('%s' %str(max_loc))
 print('%s' %str(max_val))
@@ -37,4 +77,4 @@ if max_val >= threshold:
     cv.waitKey()
 
 else:
-    print('none')
+    print('none')"""
